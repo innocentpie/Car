bf8d9eed4b70b6ad4e43b475984ad4e83b946152
Szabados Gergő <gergo.szabados11@gmail.com>
2024-05-05 21:07:04 +0200

    Add project files.

---
+++ Car.Shared/Work.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car.Shared
+{
+    public class Work
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [ForeignKey(nameof(Customer.Id))]
+        public Guid CustomerId { get; set; }
+
+        [Required]
+        [RegularExpression("^[A-Z]{3}-[1-9]{3}$")]
+        public string LicensePlate { get; set; }
+
+        [Range(typeof(DateTime), "1900-01-01", "9999-12-31")]
+        public DateTime ManufacturingDate { get; set; }
+
+        [Required]
+        public WorkCategory Category { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public int Severity { get; set; }
+
+        [Required]
+        public WorkStatus Status { get; set; }
+    }
+}
---
+++ Car.Shared/WorkCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.Shared
+{
+    public enum WorkCategory
+    {
+        Body = 0,
+        Engine = 1,
+        RunningGear = 2,
+        Breaks = 3,
+    }
+}
